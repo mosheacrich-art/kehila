5591f8381a1c8a21708af0b2bef5aea5baf9baf6
Isaac Ayash <isaacayash@Isaacs-MacBook-Air-2.local>
2026-05-19 19:09:47 +0100

    fix: replace mikve nav icon with water drop (heroicons)

--- js/nav.js
+++ js/nav.js
@@ -91,7 +91,7 @@ const NAV_ITEMS = [
         id: 'mikve',
         label: 'Mikve Barcelona',
         href: 'mikve.html',
-        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z"/><path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z"/></svg>`
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.8" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M12 3.75c-4.5 6-6.75 10.125-6.75 12.75a6.75 6.75 0 0 0 13.5 0c0-2.625-2.25-6.75-6.75-12.75Z"/></svg>`
       },
       {
         id: 'donativos',
